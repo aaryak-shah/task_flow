2a40f80d2a3a3ecc430a3780cfc6867b003190fd
Harsh Sharma <51477130+hs2361@users.noreply.github.com>
2020-07-14 21:47:56 +0530

    Refactored code

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../widgets/chart.dart';
 import '../providers/tasks.dart';
 
@@ -8,17 +9,29 @@ class TasksScreen extends StatefulWidget {
   _TasksScreenState createState() => _TasksScreenState();
 }
 
+void showNewTaskForm(BuildContext context) {
+  final tasks = Provider.of<Tasks>(context, listen: false);
+  showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: Container(),
+          behavior: HitTestBehavior.opaque,
+        );
+      });
+}
+
 class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
-    final tasks =  Provider.of<Tasks>(context);
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Column(
         children: <Widget>[
           Container(
             padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Chart(tasks.recentTasks),
+            child: Chart(),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -35,6 +48,14 @@ class _TasksScreenState extends State<TasksScreen> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 35,
+          ),
+          backgroundColor: Color(0xFF252525),
+          foregroundColor: Theme.of(context).accentColor,
+          onPressed: () => showNewTaskForm(context)),
     );
   }
 }
